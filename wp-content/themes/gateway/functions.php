3f023cdab070f83fe3e85d48c5add25906684f74
Rachaeljad <Rachaeljad@gmail.com>
2015-04-26 14:13:27 -0600

    wordpress-demo
    header styling

--- wp-content/themes/gateway/functions.php
+++ wp-content/themes/gateway/functions.php
@@ -102,6 +102,8 @@ add_action( 'widgets_init', 'gateway_widgets_init' );
 function gateway_scripts() {
 	wp_enqueue_style( 'gateway-style', get_stylesheet_uri() );
 
+	wp_enqueue_style( 'gateway-roboto', '//fonts.googleapis.com/css?family=Roboto+Slab:400,100,700,300' );
+
 	wp_enqueue_script( 'gateway-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'gateway-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
--- wp-content/themes/gateway/header.php
+++ wp-content/themes/gateway/header.php
@@ -22,15 +22,21 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'gateway' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
+		<div class="row">
 		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+        <img id="site-logo" src="<?php bloginfo('template_directory'); ?>/images/logo-header.png" alt="Your Image Description Here" />
+</a>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Primary Menu', 'gateway' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
+
+	</div> <!-- end of row -->
 	</header><!-- #masthead -->
+
+
 
 	<div id="content" class="site-content">
